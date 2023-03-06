5fc7614587fbb544c750fd5ddaf6ae91bb4763cc
santhosh101066 <ewaste101066@gmail.com>
2023-03-06 17:59:54 +0530

    Implementation in Login ,Register and Cart Count

--- app.js
+++ app.js
@@ -3,9 +3,19 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors"
 import AuthenticationRoutes from "./src/Routes/Authentication.routes.js";
+import mongodb from "./src/Services/Monogodb.services.js";
 
 const PORT= 4000
 const HOST= "localhost"
+const CLUSTER_URL="localhost:27017"
+const USERNAME=encodeURIComponent('santhosh')
+const PASSWORD=encodeURIComponent("Aspire@123") 
+const AUTH_MECHANISAM="DEFAULT"
+const DB_NAME="project"
+
+console.log('connecting');
+const status=await mongodb.connect(`mongodb://${USERNAME}:${PASSWORD}@${CLUSTER_URL}/?authMechanism=${AUTH_MECHANISAM}&authSource=${DB_NAME}`)
+!status && process.exit(-1)
 const app = express()
 app.use(cors())
 app.use(morgan('dev'))
